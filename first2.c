2a0b679e7a1fd5607ed4cb55bdc0f2835d173141
bhosalepranali <bpranali420@gmail.com>
2024-02-13 00:07:26 +0530

    first2

---
+++ first2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+
+
+
+//initialised global variable
+int No1 = 11;
+
+//non initialised global variable
+int No2;
+
+//function defination
+void Demo()
+{
+	printf("Inside Demo\n");
+}
+	
